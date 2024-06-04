9885c61d4211f17ce6d2a15351bc23c7eb388c49
Andrea Braia <andrix.braia@gmail.com>
2024-06-04 22:30:12 +0200

    Dashboard Home

--- Routes/Analytic.js
+++ Routes/Analytic.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUsersNumber } = require("../Controllers/AnalyticController");
+const {
+  getUsersNumber,
+  getStocksNumber,
+} = require("../Controllers/AnalyticController");
 
 const analyticRoutes = (db) => {
   // Funzione per ottenere tutti i prodotti dal database
   router.get("/GetUsersNumber", (req, res) => {
     getUsersNumber(res, db);
+  });
+
+  router.get("/GetStocksNumber", (req, res) => {
+    getStocksNumber(res, db);
   });
   return router;
 };
